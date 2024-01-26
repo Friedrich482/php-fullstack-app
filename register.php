4c7a8c5280df94a0f47a4290a095d24d7aebd5c5
Friedrich482 <friedricht109@gmail.com>
2024-01-26 23:18:09 +0100

    applied tailwind css directly in the login.php file 📱

--- register.php
+++ register.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration 📝</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="output.css">
     <script src="https://cdn.jsdelivr.net/npm/kute.js@2.1.2/dist/kute.min.js"></script>
     <script src="eye.js" defer></script>
 </head>
