fb3ce887b75f82116a408e8d5e3cbe08f2eba82e
Friedrich482 <friedricht109@gmail.com>
2024-01-28 16:16:53 +0100

    added a new style 🎨 to the login page form

--- login/login.php
+++ login/login.php
@@ -23,9 +23,11 @@
         <div class="w-11/12 flex flex-row flex-nowrap p-0">
             <input type="password" required name="password" id="password" placeholder="*********" class="transition duration-500 hover:scale-110 hover:border-b-4 hover:shadow-xl hover:shadow-black border-2 border-solid border-black border-b-2  border-b-purple-600 text-center bg-gray-800 text-white opacity-75 rounded-tl-md rounded-br-md h-8 w-full flex-shrink-0 box-border">
             <img src="../assets/eye/eye-crossed.png" id="eyeSlashed" alt="Eye slashed" title="Display the password" class="h-[1.25rem] w-[1.25rem] select-none relative top-1 right-6 hover:scale-125">
-        </div>        
-        
-        <input type="submit" value="Submit" id="submit" name="submit" class="cursor-pointer hover:border-purple-900 border-2 border-solid border-white rounded-2xl p-2 hover:bg-purple-200 hover:text-black transition duration-200 hover:scale-110 min-w-32 hover:shadow-sm hover:shadow-white active:bg-purple-400 active:font-thin">
+        </div>   
+
+        <div class="p-[2px] bg-gradient-to-br from-purple-700 to-red-500  rounded-2xl transition duration-500 hover:scale-110 hover:bg-gradient-to-br hover:from-red-500 hover:to-purple-700 active:bg-gradient-to-br active:from-purple-700 active:to-red-500 z-10">
+            <input type="submit" value="Submit" id="submit" name="submit" class="cursor-pointer bg-slate-700 hover:border-transparent border-2 border-solid border-transparent rounded-2xl p-2 hover:bg-slate-700 hover:text-indigo-400 transition duration-200 min-w-32 hover:shadow-sm hover:shadow-white active:font-thin active:bg-slate-800">
+        </div>
         <label id="displayErrors"></label>
         <script src="scriptLogin.js"></script>
     </form>
