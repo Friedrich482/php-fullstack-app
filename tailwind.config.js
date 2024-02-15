73ba3efd88fc9afd7753d238d793c6920326f649
Friedrich482 <friedricht109@gmail.com>
2024-02-16 00:18:49 +0100

    styled 🎨 the calculator.php file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.{html,js,php}','./login/*{html,js,php}', './home/*{html,js,php}', './include/*{html,php,js}', "./register/*{html,js,php}"],
+  content: ['./*.{html,js,php}', './space_calculator/*{html,js,php}', './login/*{html,js,php}', './home/*{html,js,php}', './include/*{html,php,js}', "./register/*{html,js,php}"],
   theme: {
     extend: {
       screens: {
         'sm-custom': '310px', 
         'mysql-custom' : '358px',
-        // 'utils-custom' : 
       },
     },
   },
