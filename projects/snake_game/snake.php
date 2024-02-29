561eb55bf7bda3c995e6f23b7421d23fb8b27c5f
Friedrich482 <friedricht109@gmail.com>
2024-02-29 23:50:34 +0100

    added typeScript to the snake game 🐍

--- projects/snake_game/snake.php
+++ projects/snake_game/snake.php
@@ -1,13 +1,13 @@
 <?php
-session_start();
-include("../../include/database.php");
+  session_start();
+  include("../../include/database.php");
 
-// Checks if the user is logged in. Otherwise, redirect him to the login page.
+  // Checks if the user is logged in. Otherwise, redirect him to the login page.
 
-if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
-    header("Location: ../../login/login.php");
-    exit;
-}
+  if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+      header("Location: ../../login/login.php");
+      exit;
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,15 +20,16 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     />
     <title>Snake Game</title>
     <link rel="stylesheet" href="../../css/style.css" />
-    <script src="script.js" defer></script>
+    <script src="../../dist/projects/snake_game/script.js" defer></script>
   </head>
 
   <body class="flex items-center justify-center flex-col gap-1 spaceImg">
-    <h1 class="text-7xl text-white">Snake Game</h1>
+    <h1 class="text-5xl text-white">Snake Game</h1>
     <canvas
       id="gameBoard"
       width="400"
       height="400"
+      
       class="mt-2 border-4 border-blue-300 rounded-2xl"
     ></canvas>
     <label id="scoreText" class="text-5xl consolas text-white">0</label>
@@ -37,5 +38,5 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 </html>
 
 <?php
-include("../../include/footer.php");
+  include("../../include/footer.php");
 ?>
