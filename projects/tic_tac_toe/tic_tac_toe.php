e342d1fe55bb372003dcc7c7408a327fbd7c1273
Friedrich482 <friedricht109@gmail.com>
2024-03-02 23:08:54 +0100

    applied default style using tailwindcss

--- projects/tic_tac_toe/tic_tac_toe.php
+++ projects/tic_tac_toe/tic_tac_toe.php
@@ -8,10 +8,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
   header("Location: ../../login/login.php");
   exit;
 }
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  header("Location: ../../home/home.php");
-  exit;
-}
+
 ?>
 
 <!DOCTYPE html>
@@ -21,24 +18,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tic Tac Toe Game</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../css/style.css">
 </head>
 
-<body>
-    <h1>Tic Tac Toe game</h1>
-    <div id="cells">
-        <div class="cell" cellIndex="0"></div>
-        <div class="cell" cellIndex="1"></div>
-        <div class="cell" cellIndex="2"></div>
-        <div class="cell" cellIndex="3"></div>
-        <div class="cell" cellIndex="4"></div>
-        <div class="cell" cellIndex="5"></div>
-        <div class="cell" cellIndex="6"></div>
-        <div class="cell" cellIndex="7"></div>
-        <div class="cell" cellIndex="8"></div>
+<body class="flex items-center justify-center flex-col gap-5 newYorkImg">
+    <h1 class="text-5xl MV-boli text-orange-400">Tic Tac Toe game</h1>
+    <div id="cells" class="border-4 border-sky-400 rounded-sm grid grid-cols-3 backdrop-blur-sm">
+        <div class="border-2 border-black text-5xl size-24 text-center MV-boli text-white select-none" cellIndex="0"></div>
+        <div class="border-2 border-black text-5xl size-24 text-center MV-boli text-white select-none" cellIndex="1"></div>
+        <div class="border-2 border-black text-5xl size-24 text-center MV-boli text-white select-none" cellIndex="2"></div>
+        <div class="border-2 border-black text-5xl size-24 text-center MV-boli text-white select-none" cellIndex="3"></div>
+        <div class="border-2 border-black text-5xl size-24 text-center MV-boli text-white select-none" cellIndex="4"></div>
+        <div class="border-2 border-black text-5xl size-24 text-center MV-boli text-white select-none" cellIndex="5"></div>
+        <div class="border-2 border-black text-5xl size-24 text-center MV-boli text-white select-none" cellIndex="6"></div>
+        <div class="border-2 border-black text-5xl size-24 text-center MV-boli text-white select-none" cellIndex="7"></div>
+        <div class="border-2 border-black text-5xl size-24 text-center MV-boli text-white select-none" cellIndex="8"></div>
     </div>
-    <label id="displayStatus">// Display the status...</label>
-    <button id="restartButton">Restart Game</button>
+    <label id="displayStatus" class="text-4xl text-center backdrop-blur-sm text-red-600">// Display the status...</label>
+    <button id="restartButton" class="outline-none text-lg bg-black rounded-lg p-1 MV-boli text-white cursor-pointer hover:scale-110 border-black border-2 active:bg-slate-900 active:outline active:outline-white active:outline-2">Restart Game</button>
     <script src="script.js"></script>
 </body>
 
