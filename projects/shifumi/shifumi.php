de416f483c754994a277e678d47b3b57c47e2b5e
Friedrich482 <friedricht109@gmail.com>
2024-03-06 00:48:29 +0100

    🎨 made the style purple

--- projects/shifumi/shifumi.php
+++ projects/shifumi/shifumi.php
@@ -9,29 +9,30 @@
     <script src="" defer></script>
 </head>
 
-<body class="flex items-center justify-center bg-gray-800 m-0">
-    <div id="gameContainer" class="flex items-center justify-center flex-col border-2 border-pink-700 gap-1 rounded-2xl bg-sky-300">
-        <div class="text-2xl consolas font-bold">
-            <p id="playerText" class=" inline-block min-w-72">Player :</p>
-            <img src="" id="playerImg" class=" inline size-12 rounded-xl border-4 inset-1 border-yellow-500"><br>
-            <p id="computerText" class=" inline-block min-w-72">Computer :</p>
-            <img src="" id="computerImg" class=" inline size-12 rounded-xl border-4 inset-1 border-yellow-500"><br>
-            <p id="resultText" class=" inline-block min-w-72 bg-blue-600">
+<body class="flex items-center justify-center flex-col bg-gray-800 pt-[20%] text-white">
+    <div id="gameContainer" class="flex items-center justify-center flex-col border-2 border-violet-700 gap-1 rounded-2xl p-4">
+        <div class="text-2xl consolas font-bold flex items-center justify-center flex-col gap-6">
+            <div class="grid grid-cols-2 grid-rows-2 gap-y-6 w-full">
+                <p id="playerText" class="text-center flex items-center justify-center">Player :</p>
+                <img src="" id="playerImg" class="ml-4 inline size-12 rounded-xl border-2 inset-1 border-violet-600">
+                <p id="computerText" class="text-center flex items-center justify-center">Computer :</p>
+                <img src="" id="computerImg" class="ml-4 inline size-12 rounded-xl border-2 inset-1 border-violet-600">
+            </div>
+            <p id="resultText" class="min-w-72">
                 Result :
                 <label id="labelResult"></label>
             </p>
-            <br>
         </div>
 
         <div id="buttons" class="flex items-center justify-center">
-            <button class=" MV-boli border-4 border-red-600 font-bold text-3xl w-32 min-w-10 rounded-xl m-1 text-white bg-black hover:border-yellow-400 bg-transparent hover:text-black backdrop-blur-sm active:border-sky-600 active:bg-black ">👊</button>
-            <button class=" MV-boli border-4 border-red-600 font-bold text-3xl w-32 min-w-10 rounded-xl m-1 text-white bg-black hover:border-yellow-400 bg-transparent hover:text-black backdrop-blur-sm active:border-sky-600 active:bg-black ">🖐</button>
-            <button class=" MV-boli border-4 border-red-600 font-bold text-3xl w-32 min-w-10 rounded-xl m-1 text-white bg-black hover:border-yellow-400 bg-transparent hover:text-black backdrop-blur-sm active:border-sky-600 active:bg-black ">✌</button>
+            <button class="border-2 border-violet-600 font-bold text-3xl w-32 min-w-10 rounded-xl m-1 text-white transiton duration-700 hover:border-white backdrop-blur-sm p-2 active:border-yellow-700">👊</button>
+            <button class="border-2 border-violet-600 font-bold text-3xl w-32 min-w-10 rounded-xl m-1 text-white transiton duration-700 hover:border-white backdrop-blur-sm p-2 active:border-yellow-700">🖐</button>
+            <button class="border-2 border-violet-600 font-bold text-3xl w-32 min-w-10 rounded-xl m-1 text-white transiton duration-700 hover:border-white backdrop-blur-sm p-2 active:border-yellow-700">✌</button>
         </div>
 
         <div class="display">
-            <p>Player's score : <span id="displayPlayerScore" class=" bg-green-500">0</span></p>
-            <p>Computer's score : <span id="displayComputerScore" class="bg-red-600">0</span></p>
+            <p>Player's score : <span id="displayPlayerScore" class="">0</span></p>
+            <p>Computer's score : <span id="displayComputerScore" class="border-black bg-slate-700 h-7 block">0</span></p>
         </div>
 
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.{html,js,php}', './projects/space_calculator/*{html,ts,php}', './projects/fetch-weather-app/*{html,ts,php}','./projects/snake_game/*{html,ts,php}','./projects/tic_tac_toe/*{html,ts,php}', './projects/shifumi/*{html,ts,php}', './login/*{html,ts,php}', './home/*{html,ts,php}', './include/*{html,php,ts}', "./register/*{html,ts,php}"],
+  content: [
+    "./*{html,ts,php}",
+    "./projects/space_calculator/*{html,ts,php}",
+    "./projects/fetch-weather-app/*{html,ts,php}",
+    "./projects/snake_game/*{html,ts,php}",
+    "./projects/tic_tac_toe/*{html,ts,php}",
+    "./projects/shifumi/*{html,ts,php}",
+    "./login/*{html,ts,php}",
+    "./home/*{html,ts,php}",
+    "./include/*{html,php,ts}",
+    "./register/*{html,ts,php}",
+  ],
   theme: {
     extend: {
       screens: {
-        'sm-custom': '310px', 
-        'mysql-custom' : '358px',
+        "sm-custom": "310px",
+        "mysql-custom": "358px",
       },
     },
   },
   plugins: [],
-}
-
+};
