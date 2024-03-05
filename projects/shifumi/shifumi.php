2facabb426790b184135b92b518c50dd121a5d38
Friedrich482 <friedricht109@gmail.com>
2024-03-05 01:24:24 +0100

    added shifumi 🖐✌👊 game to the project

---
+++ projects/shifumi/shifumi.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shifumi Game</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <img src="illustration.ppm" alt="Shifumi">
+    <div id="gameContainer">
+        <div class="display">
+            <p id="playerText">Player :</p><img src="" id="playerImg" style="display: none;"><br>
+            <p id="computerText">Computer :</p><img src=""  id="computerImg" style="display: none;"><br>
+            <p id="resultText">Result : <label id="labelResult"></label></p><br>
+        </div>
+       
+        <div id="buttons">
+            <button class = 'gameButton'>👊</button>
+            <button class = 'gameButton'>🖐</button>
+            <button class = 'gameButton'>✌</button>
+        </div>
+
+        <div class="display">
+            <p>Player's score : <span id="displayPlayerScore">0</span></p>
+            <p>Computer's score : <span id="displayComputerScore">0</span></p>
+        </div>
+
+    </div>
+</body>
+</html>
