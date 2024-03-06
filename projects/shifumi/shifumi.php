8d75db0317f54f7d621304f6fff6fb59dc7d5f44
Friedrich482 <friedricht109@gmail.com>
2024-03-06 01:39:00 +0100

    ✂ added a favicon to the tic tac toe game

--- projects/shifumi/shifumi.php
+++ projects/shifumi/shifumi.php
@@ -21,6 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Shifumi Game</title>
+    <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="../../css/style.css" />
     <script src="" defer></script>
 </head>
