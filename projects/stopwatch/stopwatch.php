d00fc30bab4edccb6922b53ffbd527670db9ee2a
Friedrich482 <friedricht109@gmail.com>
2024-03-09 03:09:01 +0100

    ✅ added minimal style to the stopwatch

--- projects/stopwatch/stopwatch.php
+++ projects/stopwatch/stopwatch.php
@@ -26,14 +26,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <script src="stopwatch.js" defer></script>
 </head>
 
-<body class="flex items-center justify-center flex-col gap-4 bg-gray-800">
-    <img src="stopwatch.png" alt="stopwatch" id="stopwatch" class="h-48 w-48">
+<body class="flex items-center justify-center flex-col gap-4 bg-gray-900">
+    <img src="./img/stopwatch.png" alt="stopwatch" id="stopwatch" class="h-48 w-48">
 
-    <div id="container" class=" rounded-md p-5 text-center bg-gray-950 w-[550px] flex items-center justify-center flex-col">
-        <div id="displayTime" class=" w-full text-justify indent-11 text-8xl text-red-600 seven-segment MV-boli">00:00:00:000</div>
-        <button id="startButton" class="text-xl rounded-lg p-1 w-full text-red-600 bg-gray-950 cursor-pointer consolas hover:border-sky-300 active:bg-sky-400">Start</button>
-        <button id="pauseButton" class="text-xl rounded-lg p-1 w-full text-red-600 bg-gray-950 cursor-pointer consolas hover:border-sky-300 active:bg-sky-400">Pause</button>
-        <button id="resetButton" class="text-xl rounded-lg p-1 w-full text-red-600 bg-gray-950 cursor-pointer consolas hover:border-sky-300 active:bg-sky-400">Reset</button>
+    <div id="container" class="flex items-center justify-center flex-col gap-4 rounded-md p-6 text-center bg-gray-950 border-2 border-violet-600">
+        <div id="displayTime" class=" w-full text-justify text-8xl text-violet-600 seven-segment MV-boli">00:00:00:000</div>
+        <div class="flex items-center justify-center gap-4">
+            <button id="startButton" class="text-xl rounded-lg p-2 w-full text-white border-2 border-violet-600 transiton duration-700 hover:border-white active:border-yellow-700 active:bg-slate-500 cursor-pointer consolas">Start</button>
+            <button id="pauseButton" class="text-xl rounded-lg p-2 w-full text-white border-2 border-violet-600 transiton duration-700 hover:border-white active:border-yellow-700 active:bg-slate-500 cursor-pointer consolas">Pause</button>
+            <button id="resetButton" class="text-xl rounded-lg p-2 w-full text-white border-2 border-violet-600 transiton duration-700 hover:border-white active:border-yellow-700 active:bg-slate-500 cursor-pointer consolas">Reset</button>
+        </div>
 
     </div>
 </body>
