66dea64787e86e791639ace3b14fbae94347b639
Friedrich482 <friedricht109@gmail.com>
2024-03-09 03:55:26 +0100

    ✅ solved the bug of overlaping milliseconds in the stopwatch

--- projects/stopwatch/stopwatch.php
+++ projects/stopwatch/stopwatch.php
@@ -26,11 +26,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <script src="../../dist/projects/stopwatch/script.js" defer></script>
 </head>
 
-<body class="flex items-center justify-center flex-col gap-4 bg-gray-900">
+<body class="flex items-center justify-center flex-col gap-6 bg-gray-900">
     <img src="./img/stopwatch.png" alt="stopwatch" id="stopwatch" class="h-48 w-48">
 
     <div id="container" class="flex items-center justify-center flex-col gap-4 rounded-lg p-6 text-center bg-gray-950 border-2 border-violet-600">
-        <div id="displayTime" class=" w-full text-justify text-8xl text-violet-600 seven-segment MV-boli">00:00:00:000</div>
+        <div id="displayTime" class=" flex justify-center items-center w-full text-8xl text-violet-600 seven-segment">
+            <span class="w-24">00</span>:
+            <span class="w-24">00</span>:
+            <span class="w-24">00</span>:
+            <span class="w-48">000</span>
+        </div>
         <div class="flex items-center justify-center gap-4">
             <button id="startButton" class="text-xl rounded-lg p-2 w-full text-white border-2 border-violet-600 transiton duration-700 hover:border-white active:border-yellow-700 active:bg-slate-500 cursor-pointer consolas">Start</button>
             <button id="pauseButton" class="text-xl rounded-lg p-2 w-full text-white border-2 border-violet-600 transiton duration-700 hover:border-white active:border-yellow-700 active:bg-slate-500 cursor-pointer consolas">Pause</button>
