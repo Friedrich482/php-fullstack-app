c830f1cfa0965f03f8b25db1802e75fe6fea190e
Friedrich482 <friedricht109@gmail.com>
2024-03-19 23:43:38 +0100

    📄 added ajax file forgottenPassword_process.php

--- login/forgottenPassword.php
+++ login/forgottenPassword.php
@@ -15,7 +15,7 @@ include "../include/header.php";
 
 <body class="flex items-center justify-center flex-wrap text-center flex-col gap-6 backgroundImg text-[whitesmoke] MV-boli">
 <p><b>Please fill this form to <i class="text-pink-500">reset your password</i></b></p>
-    <form action="login_process.php" method="post" id="loginForm" class="flex items-center justify-center flex-col bg-slate-700 opacity-95 MV-boli p-6 border-4 border-double border-purple-800 rounded-bl-[100px] rounded-tr-[100px] min-w-16 max-w-60 sm:min-w-72 sm:p-2 gap-7">
+    <form action="forgottenPassword_process.php" method="post" id="loginForm" class="flex items-center justify-center flex-col bg-slate-700 opacity-95 MV-boli p-6 border-4 border-double border-purple-800 rounded-bl-[100px] rounded-tr-[100px] min-w-16 max-w-60 sm:min-w-72 sm:p-2 gap-7">
         <h1 class=" text-4xl text-white mt-5">Reset your Password </h1>
         <label for="email" class="text-red-600 w-5/6 text-center font-bold">Please provide the email address you used when you signed up for your account</label>
         <input type="email" name="email" id="email" required placeholder="Email address..." class="transition duration-500 hover:scale-110 hover:border-b-4 hover:shadow-xl hover:shadow-black border-2 border-solid border-black border-b-2  border-b-purple-600 text-center bg-gray-800 text-white opacity-75 rounded-tl-md rounded-br-md h-8 w-11/12 sm:w-10/12 focus:outline-none text-sm sm:text-sm">
---
+++ login/forgottenPassword_process.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+include "../include/database.php";
+
+$response = ["error" => false];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+
+    if (empty($email)) {
+        $response["error"] = true;
+        $response["message"] = "The email field required !";
+    } else {
+        $stmt = pg_prepare(
+            $conn,
+            "fetch_user",
+            "SELECT * FROM users WHERE email = $1"
+        );
+        $result = pg_execute($conn, "fetch_user", [$email]);
+        if (pg_num_rows($result) === 0) {
+            $response = [
+                "error" => true,
+                "message" => "Email not found!<br> (ಥ _ ಥ)",
+            ];
+        } else {
+            $user = pg_fetch_assoc($result);
+            $_SESSION["username"] = $username;
+            $_SESSION["id"] = $user["id"];
+            $id_user = $user["id"];
+            $response = [
+                "error" => false,
+                "redirect" => "resetPassword.php?username=$username&id=$id_user",
+            ];
+        }
+        pg_free_result($result);
+        pg_close($conn);
+    }
+    echo json_encode($response);
+}
